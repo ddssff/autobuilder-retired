d5d2788efce231dc902ad00a91c9ef2a79f27b4b
David Fox <dsf@seereason.com>
2010-05-12 23:40:23 +0000

    Parameterize distribution name

--- testdebootstrap.sh
+++ testdebootstrap.sh
@@ -18,46 +18,48 @@ if [ "$UID" != "0" ] ; then
     exit 1
 fi
 
+DIST=lucid
+
 WOOT=$(mktemp -d tmpDebootStrap_XXXXXX)
 chmod og+rx ${WOOT}
 
 set -x
-debootstrap --include=perl-base --variant=buildd --components=main,restricted,universe,multiverse karmic ${WOOT} http://mirror.anl.gov/ubuntu/ 
+debootstrap --include=perl-base --variant=buildd --components=main,restricted,universe,multiverse ${DIST} ${WOOT} http://mirror.anl.gov/ubuntu/ 
 
 (cat ${WOOT}/etc/apt/sources.list | sed -e 's/^deb /deb-src /') >>${WOOT}/etc/apt/sources.list
 
 cat <<EOF >> ${WOOT}/etc/apt/sources.list
-deb http://deb.seereason.com/ubuntu karmic-seereason main
-deb-src http://deb.seereason.com/ubuntu karmic-seereason main
-deb http://mirror.anl.gov/ubuntu karmic-updates main restricted universe multiverse
-deb-src http://mirror.anl.gov/ubuntu karmic-updates main restricted universe multiverse
-deb http://mirror.anl.gov/ubuntu karmic-backports main restricted universe multiverse
-deb-src http://mirror.anl.gov/ubuntu karmic-backports main restricted universe multiverse
-deb http://mirror.anl.gov/ubuntu karmic-security main restricted universe multiverse
-deb-src http://mirror.anl.gov/ubuntu karmic-security main restricted universe multiverse
-deb file:///work/localpool karmic-seereason main
-deb-src file:///work/localpool karmic-seereason main
+deb http://deb.seereason.com/ubuntu ${DIST}-seereason main
+deb-src http://deb.seereason.com/ubuntu ${DIST}-seereason main
+deb http://mirror.anl.gov/ubuntu ${DIST}-updates main restricted universe multiverse
+deb-src http://mirror.anl.gov/ubuntu ${DIST}-updates main restricted universe multiverse
+deb http://mirror.anl.gov/ubuntu ${DIST}-backports main restricted universe multiverse
+deb-src http://mirror.anl.gov/ubuntu ${DIST}-backports main restricted universe multiverse
+deb http://mirror.anl.gov/ubuntu ${DIST}-security main restricted universe multiverse
+deb-src http://mirror.anl.gov/ubuntu ${DIST}-security main restricted universe multiverse
+deb file:///work/localpool ${DIST}-seereason main
+deb-src file:///work/localpool ${DIST}-seereason main
 EOF
 
 chroot ${WOOT} apt-get update
 
 
-info 'Replacing ~/.autobuilder/dists/karmic-seereason/clean-Moderate'
-rm -rf ~/.autobuilder/dists/karmic-seereason/clean-Moderate
-mv ${WOOT} ~/.autobuilder/dists/karmic-seereason/clean-Moderate
+info "Replacing ~/.autobuilder/dists/${DIST}-seereason/clean-Moderate"
+rm -rf ~/.autobuilder/dists/${DIST}-seereason/clean-Moderate
+mv ${WOOT} ~/.autobuilder/dists/${DIST}-seereason/clean-Moderate
 
-cat <<EOF >> ~/.autobuilder/dists/karmic-seereason/sources
-deb http://deb.seereason.com/ubuntu karmic-seereason main
-deb-src http://deb.seereason.com/ubuntu karmic-seereason main
-deb http://mirror.anl.gov/ubuntu karmic-updates main restricted universe multiverse
-deb-src http://mirror.anl.gov/ubuntu karmic-updates main restricted universe multiverse
-deb http://mirror.anl.gov/ubuntu karmic-backports main restricted universe multiverse
-deb-src http://mirror.anl.gov/ubuntu karmic-backports main restricted universe multiverse
-deb http://mirror.anl.gov/ubuntu karmic-security main restricted universe multiverse
-deb-src http://mirror.anl.gov/ubuntu karmic-security main restricted universe multiverse
+cat <<EOF >> ~/.autobuilder/dists/${DIST}-seereason/sources
+deb http://deb.seereason.com/ubuntu ${DIST}-seereason main
+deb-src http://deb.seereason.com/ubuntu ${DIST}-seereason main
+deb http://mirror.anl.gov/ubuntu ${DIST}-updates main restricted universe multiverse
+deb-src http://mirror.anl.gov/ubuntu ${DIST}-updates main restricted universe multiverse
+deb http://mirror.anl.gov/ubuntu ${DIST}-backports main restricted universe multiverse
+deb-src http://mirror.anl.gov/ubuntu ${DIST}-backports main restricted universe multiverse
+deb http://mirror.anl.gov/ubuntu ${DIST}-security main restricted universe multiverse
+deb-src http://mirror.anl.gov/ubuntu ${DIST}-security main restricted universe multiverse
 EOF
 
 info 'Starting autobuilder with new root'
-autobuilder karmic-seereason --force haskell-devscripts --target haskell-devscripts
+autobuilder ${DIST}-seereason --force haskell-devscripts --target haskell-devscripts
 
 exit 0
