0d334233ad55282acdfabaab596f8cac554953a8
David Fox <dsf@seereason.com>
2010-09-01 13:47:26 +0000

    Fix the computation of the revision string for apt targets (how did this get broken?)

--- Debian/AutoBuilder/BuildTarget/Apt.hs
+++ Debian/AutoBuilder/BuildTarget/Apt.hs
@@ -32,7 +32,7 @@ documentation = [ "apt:<distribution>:<packagename> - a target of this form look
 -- of a package if it is already in the apt repository.
 instance BuildTarget Apt where
     getTop _ (Apt _ _ _ t) = topdir t
-    revision _ (Apt d p (Just v) _) = fail $ "apt:" ++ (sliceName . sliceListName $ d) ++ ":" ++ p ++ "=" ++ show v
+    revision _ (Apt d p (Just v) _) = return $ "apt:" ++ (sliceName . sliceListName $ d) ++ ":" ++ p ++ "=" ++ show v
     revision _ (Apt _ _ Nothing _) = fail "Attempt to generate revision string for unversioned apt package"
     logText (Apt name _ _ _) _ = "Built from " ++ sliceName (sliceListName name) ++ " apt pool"
 
