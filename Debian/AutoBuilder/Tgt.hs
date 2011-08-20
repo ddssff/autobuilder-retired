b5810e23cd8851b80cee5c817e639ce2fec4a3f1
David Fox <dsf@seereason.com>
2011-08-20 18:54:08 +0000

    Make Tgt an instance of BuildTarget

--- Debian/AutoBuilder/Tgt.hs
+++ Debian/AutoBuilder/Tgt.hs
@@ -3,7 +3,7 @@ module Debian.AutoBuilder.Tgt
     ( Tgt(Tgt)
     ) where
 
-import Debian.AutoBuilder.BuildTarget.Common (BuildTarget)
+import Debian.AutoBuilder.BuildTarget.Common (BuildTarget(..))
 
 -- | Objects of type Tgt contain an instance of the BuildTarget type
 -- class.
@@ -14,3 +14,11 @@ instance Show Tgt where
 
 --getSourceTree' :: Tgt -> SourceTree
 --getSourceTree' (Tgt a) = getSourceTree a
+
+instance BuildTarget Tgt where
+    getTop params (Tgt x) = getTop params x
+    cleanTarget params (Tgt x) path = cleanTarget params x path
+    revision params (Tgt x) = revision params x
+    buildWrapper params os tree status (Tgt x) action = buildWrapper params os tree status x action
+    logText (Tgt x) result = logText x result
+    mVersion (Tgt x) = mVersion x
