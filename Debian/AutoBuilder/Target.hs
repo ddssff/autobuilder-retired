4fe0147941b229e44ab4b10015066461ecbf1b79
David Fox <dsf@seereason.com>
2011-10-14 17:45:12 +0000

    Install recommended build dependencies so we can create links between doc modules

--- Debian/AutoBuilder/Target.hs
+++ Debian/AutoBuilder/Target.hs
@@ -811,7 +811,7 @@ downloadDependencies os source extra versions =
       command = ("export DEBIAN_FRONTEND=noninteractive; " ++
                  (if True then aptGetCommand else pbuilderCommand))
       pbuilderCommand = "cd '" ++  path ++ "' && /usr/lib/pbuilder/pbuilder-satisfydepends"
-      aptGetCommand = "apt-get --yes --force-yes install -o APT::Install-Recommends=False --download-only " ++ intercalate " " (map showPkgVersion versions ++ extra)
+      aptGetCommand = "apt-get --yes --force-yes install -o APT::Install-Recommends=True --download-only " ++ intercalate " " (map showPkgVersion versions ++ extra)
       path = pathBelow (rootPath root) (topdir source)
       root = rootDir os
 
@@ -833,7 +833,7 @@ installDependencies os source extra versions =
       command = ("export DEBIAN_FRONTEND=noninteractive; " ++
                  (if True then aptGetCommand else pbuilderCommand))
       pbuilderCommand = "cd '" ++  path ++ "' && /usr/lib/pbuilder/pbuilder-satisfydepends"
-      aptGetCommand = "apt-get --yes --force-yes install -o APT::Install-Recommends=False " ++ intercalate " " (map showPkgVersion versions ++ extra)
+      aptGetCommand = "apt-get --yes --force-yes install -o APT::Install-Recommends=True " ++ intercalate " " (map showPkgVersion versions ++ extra)
       --aptGetCommand = "apt-get --yes build-dep -o APT::Install-Recommends=False " ++ sourcpackagename
       path = pathBelow (rootPath root) (topdir source)
       root = rootDir os
