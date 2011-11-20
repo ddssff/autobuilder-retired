95ed8b14a4cb68f97bb595407660a155b3b4d1f6
David Fox <dsf@seereason.com>
2011-11-20 19:17:33 +0000

    Update doc for DebVersion to reflect change in cabal-debian

--- Debian/AutoBuilder/Params.hs
+++ Debian/AutoBuilder/Params.hs
@@ -79,7 +79,7 @@ instance Show Strictness where
 data PackageFlag
     = RelaxDep String		-- ^ Build dependencies which be ignored when deciding whether to rebuild
     | ExtraDep String		-- ^ Build dependencies which should be added to the debian/control file
-    | DebVersion String         -- ^ The exact debian version number to insert into the changelog.
+    | DebVersion String         -- ^ The exact debian version number to insert into the changelog.  An exception will be thrown if the hackage version looks newer than this.
     | Revision String           -- ^ Pass --revision <string> to cabal-debian so a suffix will be added to the cabal version to get the debian version.  By default this is -1~hackage1.  Debian policy says this should either be empty or begin with a dash.
     | Epoch Int                 -- ^ Set the epoch number in the version number
     | Patch B.ByteString        -- ^ Apply the patch
