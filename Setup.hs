6b34c9f52d4bb5e3096bd332d61110eb927b8d81
David Fox <dsf@seereason.com>
2012-10-29 19:57:35 +0000

    Add new debianization code to Setup.hs

--- Setup.hs
+++ Setup.hs
@@ -1,12 +1,31 @@
 #!/usr/bin/runhaskell
 
 import Control.Exception (SomeException)
+import Distribution.Debian (debianize, Flags(..), Executable(..), defaultFlags)
 import Distribution.Simple
+import Distribution.Simple.LocalBuildInfo (LocalBuildInfo(buildDir))
 import System.Cmd
 import System.Exit
 
 main = defaultMainWithHooks simpleUserHooks {
-         postBuild = \ _ _ _ _ -> runTestScript
+         postConf = \ _ _ _ lbi -> debianize (defaultFlags { executablePackages = [Script "autobuilder" "autobuilder"]
+                                                           , binaryPackageDeps = map (\ p-> ("autobuilder",p))
+                                                                                     [ "libghc-autobuilder-dev (= ${Source-Version})"
+                                                                                     , "debootstrap"
+                                                                                     , "rsync"
+                                                                                     , "dupload"
+                                                                                     , "darcs"
+                                                                                     , "tla"
+                                                                                     , "mercurial"
+                                                                                     , "subversion"
+                                                                                     , "apt"
+                                                                                     , "build-essential"
+                                                                                     , "quilt"
+                                                                                     , "curl"
+                                                                                     , "cabal-debian (>= 2)" ]
+                                                           , debMaintainer = Just "David Fox <dsf@seereason.com>"
+                                                           , dryRun = case buildDir lbi of "dist/build" -> True; "dist-ghc/build" -> False })
+       , postBuild = \ _ _ _ _ -> runTestScript
        , runTests = \ _ _ _ _ -> runTestScript
        }
 
